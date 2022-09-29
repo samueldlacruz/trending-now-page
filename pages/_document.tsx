355ca95866a42fbe497593b14a45762cdd245272
Samuel De la Cruz <s.delacruz@tele.finance>
2022-09-28 21:11:46 -0400

    refactor: _document file updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,30 +1,15 @@
-import Document, { Html, Main, Head, NextScript } from 'next/document';
-import { Children } from 'react';
+import { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
-    static async getInitialProps(ctx: any) {
-        const initialProps = await Document.getInitialProps(ctx);
-
-        return {
-            ...initialProps,
-            styles: [...Children.toArray(initialProps.styles)],
-        }
-    }
-
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <link rel="shortcut icon" href='/favicon.png' type="image/png" />
-                    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet" />
-                </Head>
-                <body className="hidden-overflow">
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
-
-export default MyDocument;
