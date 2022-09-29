de44d3d181975eec858909806287b3dda44c9a18
Samuel De la Cruz <s.delacruz@tele.finance>
2022-09-28 23:09:12 -0400

    feat: 404 page added

--- components/Inform.tsx
+++ components/Inform.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Inform = ({ width = "8rem", message }: { width?: number | string, message: string }) => {
 
     return (
-        <div className="flex  pt-4 flex-col items-center">
-            <i className={`ri-article-line md:text-[${width}] text-[6rem] text-white`}></i>
+        <div className="flex pt-4 flex-col items-center">
+            <i className={`ri-article-line sm:text-[6rem] text-white`} style={{fontSize: width}}></i>
             <span className="md:text-4xl text-3xl mt-5 text-center text-white/95">{message}</span>
         </div>
     )
---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import Inform from "../components/inform";
+
+const NotFound = () => {
+    return (
+        <div className="flex min-h-screen mt-10 mb-10 relative flex-col">
+            <Inform width={"15rem"} message="Page not found" />
+        </div>
+    )
+}
+export default NotFound;
