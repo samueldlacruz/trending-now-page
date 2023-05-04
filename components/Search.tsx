fccd474d7a6b6cd33b2c84e53b7d9b9ff1a6f4b6
Samuel De la Cruz <samueldlacg@gmail.com>
2023-05-04 15:10:20 -0400

    chore: improve i11y and SEO in search input

--- components/Search.tsx
+++ components/Search.tsx
@@ -11,14 +11,15 @@ const Search = () => {
     return (
         <div className="mt-4 flex w-full">
             <input
-                type="text"
+                type="search"
                 value={terms}
+                id="search"
                 onChange={(ev: React.ChangeEvent<HTMLInputElement>) => setTerms(ev.target.value)}
                 className="border w-11/12 text-white font-mono rounded-sm py-2 px-2 border-slate-600/60 bg-white/20"
                 placeholder="Search anything, Example: Tesla"
             />
 
-            <button onClick={() => dispatch(searchByQuery(terms))} className="bg-red-700 hover:bg-red-800 cursor-pointer text-white rounded-sm ml-2 py-2 px-4">
+            <button aria-label="Search" onClick={() => dispatch(searchByQuery(terms))} className="bg-red-700 hover:bg-red-800 cursor-pointer text-white rounded-sm ml-2 py-2 px-4">
                 <i className="ri-search-line"></i>
             </button>
         </div>
