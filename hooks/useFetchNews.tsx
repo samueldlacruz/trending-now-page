bea34e661b3b29d96f36aa36ece360b4c10fcdd1
Samuel De la Cruz <samueldlacg@gmail.com>
2023-05-04 14:50:26 -0400

    fix: fixed use fetch news hook

--- hooks/useFetchNews.tsx
+++ hooks/useFetchNews.tsx
@@ -20,6 +20,8 @@ export const useFetchNews = () => {
 
             if (response.status === "error") {
                 dispatch(handleError(response.message!))
+                dispatch(updateLoading(false))
+                
                 return
             }
 
